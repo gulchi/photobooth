b63e70abf3b28a140ff22350eb57a371ab2c5848
SL TecH2 Christian Gulde <christian.gulde@sl-tech2.de>
2019-01-05 18:44:26 +0100

    Used mkstemp for print-hook.py

--- software/print-hook.py
+++ software/print-hook.py
@@ -4,11 +4,15 @@ import os
 import sys
 import cups
 import time
+import tempfile
 
 from subprocess import call
 
+new_file, output_file = tempfile.mkstemp(dir="/dev/shm/", suffix=".jpg", prefix="print-hook")
 
-output_file = "/dev/shm/print-tile.jpg"
+
+
+#output_file = "/dev/shm/print-tile.jpg"
 
 np = len(sys.argv) -1
 
@@ -19,10 +23,7 @@ if np < 1:
 
 if np == 4:
 
-    #call(["montage", sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], "-tile", "2x2", "-geometry" ,"1804x1232+20+20", output_file])
     call(["montage", sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], "-tile", "2x2", "-geometry" ,"2460x1846+20+20", output_file])
-    #call(["montage", sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], "-tile", "2x2", "-geometry" ,"2460x1846", output_file])
-    #print "Finished"
     conn = cups.Connection()
     printers = conn.getPrinters()
     printer_name = printers.keys()[0]
@@ -33,9 +34,21 @@ if np == 4:
 
     print_id = conn.printFile(printer_name, output_file, "Photo Booth", {})
     # Wait until the job finishes
-    while conn.getJobs().get(print_id, None):
-        time.sleep(1)
-        os.unlink(output_file)
+    counter=1
+    waiter=True
+    while waiter
+	    counter+=1
+		
+		if conn.getJobs().get(print_id, None):
+		    waiter = False
+			
+		if counter > 10:
+		    waiter = False
+			
+		time.sleep(10)	
+    
+    time.sleep(10)
+    os.unlink(output_file)
 
 
 
